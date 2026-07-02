dce2e88b37db623f9236e8276561b33c0524f04d
dljay <jlitzon@gmail.com>
2026-07-03 11:02:17 +1200

    12 simple food compelted

--- 12-Simple Food restaurant/App.tsx
+++ 12-Simple Food restaurant/App.tsx
@@ -1,3 +1,13 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+
 export default function App() {
-    return null
+  return (
+    <div>
+      <Header></Header>
+      <MainContent></MainContent>
+      <Footer></Footer>
+    </div>
+  );
 }
---
+++ 12-Simple Food restaurant/components/MainContent.tsx
@@ -0,0 +1,20 @@
+export default function MainContent() {
+  return (
+    <main>
+      <h1 className="logo-container">Simple Restaurant</h1>
+      <p className="info-container">We make simple food for simple people. Want a sandwich? You got it. A hot dog? Coming right up. Poached salmon eggs with fire-roasted asparagus fritters hand-glazed in locally-sourced artisanal honey? Get out of here. Try the Fancy Food Cafe across the street.</p>
+
+      <div className="image-container">
+        <div className="image-wrapper">
+          <img className="hot-dog" src="images/hotdogs.jpg" alt="Hot dogs" />
+        </div>
+        <div className="image-wrapper">
+          <img src="images/sandwich.jpg" alt="Sandwich" />
+        </div>
+        <div className="image-wrapper">
+          <img src="images/hamburger.jpg" alt="Hamburger" />
+        </div>
+      </div>
+    </main>
+  );
+}
